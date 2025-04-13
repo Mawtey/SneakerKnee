f31df59924f6be665f90f71561d1bc59c82ccd2d
Mawtey <mawtey@users.noreply.github.com>
2025-04-13 16:00:20 +0400

    Tests

--- src/main/java/com/example/demo/model/Review.java
+++ src/main/java/com/example/demo/model/Review.java
@@ -8,16 +8,7 @@ public class Review {
     private Integer id;
     private Integer productId;
     private Integer userId;
-
-    @Setter
     private Integer rating;
-
     private String comment;
 
-    public void setRating(Integer rating) {
-        if (rating < 1 || rating > 5) {
-            throw new IllegalArgumentException("Rating must be between 1 and 5");
-        }
-        this.rating = rating;
-    }
 }
