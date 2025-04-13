aa1b6680b7dbdaf7e98cc6b6a0ffa25b02fa7ef4
Mawtey <mawtey@users.noreply.github.com>
2025-04-13 14:37:07 +0400

    Project creation

---
+++ src/main/java/com/example/demo/SneakerKneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerKneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerKneeApplication.class, args);
+	}
+
+}
